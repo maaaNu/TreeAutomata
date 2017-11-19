aa29a6004ef59a25bdb7988c115f5d50c2350e71
maaanu <hr.kniep@googlemail.com>
2017-11-19 23:37:38 +0100

    still working... tests & description is still missing. also will do some refactoring (it doesn't look a lot like scala right now :-(

--- src/main/scala/de/university_stuttgart/maaanu/Main.scala
+++ src/main/scala/de/university_stuttgart/maaanu/Main.scala
@@ -1,8 +1,9 @@
 package de.university_stuttgart.maaanu
 
-import de.university_stuttgart.maaanu.parsers.{DeleteTraceParser, HeightParser, ProdRuleParser}
+import de.university_stuttgart.maaanu.parsers._
 
 import scala.collection.JavaConverters._
+import scala.collection.immutable.Seq
 import scala.io.Source
 import scala.util.Try
 
@@ -13,18 +14,19 @@ object Main extends App {
     case "2" => prodRulesParser()
     case "3" => deleteTraceParser()
     case "4" => prodRulesWithFreqParser()
+    case "5" => lex2RulesParser()
     case _ => println("This Task is unknown!")
   }
 
-  private def heightParser() = {
-    for (line <- readResource()) {
+  private def heightParser(): Unit = {
+    for (line <- readTreeResource()) {
       println(HeightParser.parseAll(HeightParser.start, line).getOrElse(-1))
     }
   }
 
-  private def prodRulesParser() = {
+  private def prodRulesParser(): Unit = {
     val parser = new ProdRuleParser()
-    for(line <- readResource()) {
+    for (line <- readTreeResource()) {
       parser.parseAll(parser.start, line)
     }
 
@@ -33,18 +35,46 @@ object Main extends App {
     }
   }
 
-  private def deleteTraceParser() = {
-    for (line <- readResource()) {
+  private def deleteTraceParser(): Unit = {
+    for (line <- readTreeResource()) {
       println(DeleteTraceParser.parseAll(DeleteTraceParser.start, line).getOrElse("FAIL"))
     }
   }
 
-  private def prodRulesWithFreqParser() = {
+  private def prodRulesWithFreqParser(): Unit = {
+    val parser = new FreqProductionParser()
 
+    for (line <- readTreeResource()) {
+      parser.parseAll(parser.start, line)
+    }
+
+    for ((k, v) <- parser.prodMap.asScala) {
+      for (prod <- v.asScala) println(parser.freqMap.get((k, prod)) + " " + k + " " + prod)
+    }
   }
 
-  private def readResource() = {
-    val resource : String = Try(args(1)) getOrElse  "/wsj_0100.tree"
+  private def lex2RulesParser(): Unit = {
+    val parser = new Lex2RulesParser()
+
+    for (line <- readNoTraceTreeResource()) {
+      parser.parseAll(parser.start, line)
+    }
+
+    for ((k, v) <- parser.rewrite_map().asScala) {
+      println(k + "\t" + v.asScala.map(s => s._1 + " " + s._2).mkString(" "))
+    }
+  }
+
+  private def readTreeResource(): Seq[String] = {
+    readFileOrElse("/wsj_0100.tree")
+  }
+
+  private def readNoTraceTreeResource(): Seq[String] = {
+    readFileOrElse("/wsj_0100.noTrace")
+  }
+
+  private def readFileOrElse(file: String): Seq[String] = {
+    val resource: String = Try(args(1)) getOrElse file
     Source.fromURL(getClass.getResource(resource)).getLines().toList
   }
 }
